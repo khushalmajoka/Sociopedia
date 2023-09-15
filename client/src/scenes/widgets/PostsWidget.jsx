c6c71de564e1c0229a7182802815c0b8d87a4ba5
Khushal <khushalmajoka21@gmail.com>
2023-09-15 16:41:35 +0530

    fixed posts url

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -9,7 +9,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   const token = useSelector((state) => state.token);
 
   const getPosts = async () => {
-    const response = await fetch(`${process.env.REACT_APP_SERVER_URL}`, {
+    const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/posts`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
@@ -19,7 +19,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
 
   const getUserPosts = async () => {
     const response = await fetch(
-      `${process.env.REACT_APP_SERVER_URL}/posts/${userId}/posts`,
+      `/posts/${userId}/posts`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
